4dfcba3c6560067299ec51168e0b1c612f2b6c1a
alexy4744 <alexy4744@gmail.com>
2019-05-07 17:01:08 -0400

    fix navbar

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const appContainer = $(".app-container");
-const appNav = $(".app-nav");
 const bgDimmer = $(".app-bg-dimmer");
 
 $(".nav-toggler")
   .hover(() => bgDimmer.css("border-width", "75px 50px"))
-  .mouseleave(() => bgDimmer.css("border-width", "10px"))
-  .click(function () {
-    const toggler = $(this);
-
-    if (!toggler.hasClass("nav-opened")) {
-      toggler.addClass("nav-opened");
-      appContainer.css("transform", "translateY(40%)");
-    } else {
-      toggler.removeClass("nav-opened");
-      appContainer.css("transform", "initial");
-    }
-  });
+  .mouseleave(() => bgDimmer.css("border-width", "5px"))
+  .click(() => $(".nav-wrapper").toggleClass("nav-opened"));
 
 $(".app-container").scroll(function (scrollEvent) {
 
