d83e7c319f5cec0b87523b8f8e8d077b2eb4150d
alexy4744 <alexy4744@gmail.com>
2019-05-06 22:49:26 -0400

    add basic nav transitions

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// const appContainer = $(".app-container");
+const appContainer = $(".app-container");
+const appNav = $(".app-nav");
 const bgDimmer = $(".app-bg-dimmer");
 
 $(".nav-toggler")
   .hover(() => bgDimmer.css("border-width", "75px 50px"))
   .mouseleave(() => bgDimmer.css("border-width", "10px"))
-  .click(function() {
+  .click(function () {
+    const toggler = $(this);
 
+    if (!toggler.hasClass("nav-opened")) {
+      toggler.addClass("nav-opened");
+      appContainer.css("transform", "translateY(40%)");
+    } else {
+      toggler.removeClass("nav-opened");
+      appContainer.css("transform", "initial");
+    }
   });
 
-$(".app-container").scroll(function(scrollEvent) {
+$(".app-container").scroll(function (scrollEvent) {
 
 });
